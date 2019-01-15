e6ecbb65ec02319db5a9e8263cc6bd1f0c97b983
Shelan <shelandebesai@gmail.com>
2019-01-15 14:23:41 -0800

    setup

---
+++ index.js
@@ -0,0 +1,16 @@
+// import { Navigation } from "react-native-navigation";
+// import App from "./App";
+//
+//
+//
+// Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+//
+// Navigation.events().registerAppLaunchedListener(() => {
+//   Navigation.setRoot({
+//     root: {
+//       component: {
+//         name: "navigation.playground.WelcomeScreen"
+//       }
+//     }
+//   });
+// });
